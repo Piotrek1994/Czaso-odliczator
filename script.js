7546adaec9bf3304e83ba787ab2481ebdd97149d
Piorek1994 <pprus94@gmail.com>
2022-04-16 11:06:16 +0200

    Project not finished, settings panel need to be fixed

--- script.js
+++ script.js
@@ -18,11 +18,29 @@ const eventSpan = document.querySelector('.event')
 let userTime
 
 
+const setTime = () => {
+	const currentTime = new Date()
+	const result = userTime - currentTime
+
+	const days = Math.floor(result / 1000 / 60 / 60 / 24)
+	const hours = Math.floor(result / 1000 / 60 / 60 ) %60
+	const minutes = Math.floor(result / 1000 / 60) %60
+	const seconds = Math.floor(result / 1000 ) %60
+
+	daysCount.textContent = days
+	hoursCount.textContent = hours
+	minutesCount.textContent = minutes
+	secondsCount.textContent = seconds
+}
+
+
 const appUpdate = () => {
 	eventSpan.textContent = eventName.value
 	userTime = new Date(`${eventMonth.value} ${eventDay.value} ${eventYear.value}`)
 	imageSection.style.backgroundImage = `url('${eventImg.value}')`
 	console.log(userTime);
+	setTime()
+	
 }
 
 settingsBtn.addEventListener('click', () => {
@@ -30,7 +48,8 @@ settingsBtn.addEventListener('click', () => {
 })
 
 
-
+appUpdate()
+setInterval(setTime, 1000)
 
 
 
